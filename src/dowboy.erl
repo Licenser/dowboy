1146b442eb9453d49adb578979fa5d6063f731b1
Heinz N. Gies <heinz@licenser.net>
2013-01-28 17:31:34 +0100

    Added load example.

--- src/dowboy.erl
+++ src/dowboy.erl
@@ -1,9 +1,20 @@
 -module(dowboy).
 
--export([start/0]).
+-export([start/0, load/1]).
 
 start() ->
     ok = application:start(erltrace),
     ok = application:start(jsx),
     ok = application:start(cowboy),
     application:start(dowboy).
+
+
+fibo(0) ->
+    0;
+fibo(1) ->
+    1;
+fibo(N) when N > 1 ->
+    fibo(N-1) + fibo(N-2).
+
+load(N) ->
+    timer:tc(fun fibo/1, [N]).
